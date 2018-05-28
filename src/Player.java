b3e20772c60268a8a0b4b19ae071a0948521f8ef
rishavb123 <rishavb123@gmail.com>
2018-05-27 20:50:06 -0400

    working on spritesheet

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,23 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Player extends Entity {
 
 	private String name;
 	private SpriteSheet spriteSheet;
+	private ArrayList<BufferedImage[]> sprites;
+	private final int[] numFrames = {
+			1, 4, 7, 3, 4, 3
+	};
+	
+	private static final int IDLE = 0;
+	private static final int WALKING = 1;
+	private static final int LASER = 2;
+	private static final int JUMP = 3;
+	private static final int SHEILD = 4;
+	private static final int ATTACK = 5;
 	
 	public Player(String name, int x, int y) 
 	{
@@ -29,7 +42,7 @@ public class Player extends Entity {
 
 	@Override
 	public void draw(GraphicsContext gc) {
-		spriteSheet.draw();
+
 	}
 
 }
--- src/SpriteSheet.java
+++ src/SpriteSheet.java
@@ -1,48 +1,72 @@
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 
 public class SpriteSheet {
 	
-	private BufferedImage bufferedImage;
-//	private String path;
-	private int width;
-	private int height;
-	private int[] pixels;
+	private BufferedImage[] frames;
+	private int currentFrame;
+	
+	private long startTime;
+	private long delay;
+	
+	private boolean playedOnce;
 
 	public SpriteSheet(String path)
 	{
-//		this.path = path;
-		try {
-			bufferedImage = ImageIO.read(SpriteSheet.class.getResourceAsStream(path));
-		} catch (IOException e) {
-			e.printStackTrace();
+		playedOnce = false;
+	}
+	
+	public void setFrames(BufferedImage[] frames) 
+	{
+		this.frames = frames;
+		currentFrame = 0;
+		startTime = System.nanoTime();
+		playedOnce = false;
+	}
+	
+	public void setDelay(long d)
+	{
+		delay = d;
+	}
+	
+	public void setFrame(int i)
+	{
+		currentFrame = i;
+	}
+	
+	public void update()
+	{
+		if(delay == -1)
+			return;
+		
+		long elapsed = (System.nanoTime() - startTime) / 1000000;
+		
+		if(elapsed > delay)
+		{
+			currentFrame++;
+			startTime = System.nanoTime();
 		}
 		
-		this.width = bufferedImage.getWidth();
-		this.height = bufferedImage.getHeight();
-		
-		pixels = bufferedImage.getRGB(0, 0, width, height, null, 0, width);
-		
-		for(int i=0; i < 8;i++)
-			System.out.println(pixels[i]);
-		
-		for(int i=0; i< pixels.length;i++)
-			pixels[i] = (pixels[i]&0xff)/64;
-		
-		System.out.println();
-
-		for(int i=0; i < 8;i++)
-			System.out.println(pixels[i]);
-		
+		if(currentFrame == frames.length)
+		{
+			currentFrame = 0;
+			playedOnce = true;
+		}
 		
 	}
 	
-	public void draw() {
-		
+	public int getFrame()
+	{
+		return currentFrame;
 	}
 	
-
+	public BufferedImage getImage()
+	{
+		return frames[currentFrame];
+	}
+	
+	public boolean hasPlayedOnce()
+	{
+		return playedOnce;
+	}
+	
 }
